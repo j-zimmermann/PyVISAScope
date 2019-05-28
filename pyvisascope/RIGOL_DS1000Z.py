0853c98fed807ff1c8321f77e598e5eabb11eee7
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2019-05-28 17:40:11 +0200

    license header added

--- pyvisascope/RIGOL_DS1000Z.py
+++ pyvisascope/RIGOL_DS1000Z.py
@@ -1,4 +1,24 @@
-from tekVisa import logger, scope, convUnicodeToAscii
+"""
+    PyVISAScope provides an interface to different scopes.
+    Copyright (C) 2016 Ye-Sheng Kuo <yesheng.kuo@gmail.com>
+    Copyright (C) 2019 Julius Zimmermann <julius.zimmermann@uni-rostock.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+from pyvisascope import logger, scope, convUnicodeToAscii
 
 
 class WaveformFormat(object):
