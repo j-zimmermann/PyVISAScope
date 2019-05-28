b21294163432c9ca2f9145f47531f51aa0d2c078
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2019-05-28 16:15:46 +0200

    restructure project

--- pyvisascope/RIGOL_DS1000Z.py
+++ pyvisascope/RIGOL_DS1000Z.py
@@ -1,44 +1,4 @@
-"""
-    PyVISAScope provides an interface to different scopes.
-    Copyright (C) 2016 Ye-Sheng Kuo <yesheng.kuo@gmail.com>
-    Copyright (C) 2019 Julius Zimmermann <julius.zimmermann@uni-rostock.de>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
-import visa
-import unicodedata
-import logging
-import sys
-
-
-if sys.platform == 'linux':
-    RESOURCE_MANAGER = visa.ResourceManager('@py')
-elif sys.platform == 'win32':
-    RESOURCE_MANAGER = visa.ResourceManager()
-
-logger = logging.getLogger('pyvisa')
-
-
-def convUnicodeToAscii(s):
-        return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
-
-
-def getDeviceList():
-        tmp = list(RESOURCE_MANAGER.list_resources())
-        tmp = [convUnicodeToAscii(i) for i in tmp]
-        return tmp
+from tekVisa import logger, scope, convUnicodeToAscii
 
 
 class WaveformFormat(object):
@@ -69,20 +29,9 @@ class WaveformFormat(object):
                                 logger.debug('Set Time Div to {}'.format(self.dictionary['Time Div']))
 
 
-class scope(object):
+class DS1000Z(scope):
         def __init__(self, resource):
-                self.myScope = RESOURCE_MANAGER.open_resource(resource)
-                self.mkdir = True
-                self.counter = 0
-
-        def checkChannel(self, channel):
-                answer = False
-                if channel == 'CH1' or channel == 'CH2' or channel == 'CH3' or channel == 'CH4':
-                        answer = True
-                if answer is False:
-                        logger.error('Choose one of the four channels, e.g. CH1')
-                        return answer
-                return answer
+                super().__init__(resource)
 
         def set_channel(self, channel):
                 if self.checkChannel(channel):
---
+++ pyvisascope/__init__.py
@@ -0,0 +1,3 @@
+from .pyvisascope import scope, get_resources
+from .TDS2000 import TDS2000
+from RIGOL_DS1000Z import DS1000Z
