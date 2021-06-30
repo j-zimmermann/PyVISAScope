71b856067c6df441ae19c60712a8d742b86dcbe8
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2021-06-30 12:38:01 +0200

    typo

--- pyvisascope/__init__.py
+++ pyvisascope/__init__.py
@@ -1,6 +1,6 @@
 from .pyvisascope import scope, get_resources
 from .TDS2000 import TDS2000
 from .RIGOL_DS1000Z import DS1000Z
-from -.RIGOLMS05000 import MSO5000 
+from .RIGOLMS05000 import MSO5000
 from .SIGLENT import SIGLENT
 from .SDS_1104X_E import SDS_1104X_E
